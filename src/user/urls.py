cc93b731f9582fbf385ffdc11c415b06bea34f9b
SSK19 <salmansiddik19@gmail.com>
2020-11-02 16:04:31 +0600

    initial commit

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from user.views import LoginView, UserDRFCreateView
+from user.views import LoginView, UserListView, TeacherCreateView, StudentCreateView
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='user-login-view'),
-    path('create-list', UserDRFCreateView.as_view(),
-         name='user-create-list-view'),
+    path('list', UserListView.as_view(), name='user-list-view'),
+    path('teacher/create-list', TeacherCreateView.as_view(),
+         name='teacher-create-list-view'),
+    path('student/create-list', StudentCreateView.as_view(),
+         name='student-create-list-view'),
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -3,8 +3,10 @@ import json
 from django.views import View
 from django.http import JsonResponse
 from django.conf import settings
+from django.db import IntegrityError
+from django.contrib.auth.models import Group
 
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListAPIView
 
 from .serializers import UserSerializer
 
@@ -39,6 +41,74 @@ class LoginView(View):
             return JsonResponse({'massage': 'can not find user'}, status=404)
 
 
-class UserDRFCreateView(ListCreateAPIView):
+class UserListView(ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.filter()
+
+
+class TeacherCreateView(View):
+    @staticmethod
+    def _serializer(user):
+        return {
+            'id': user.id,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'is_teacher': user.is_teacher,
+            'groups': [group.name for group in user.groups.all()]
+        }
+
+    def post(self, request):
+        body = json.loads(request.body.decode('utf-8'))
+        try:
+            user = User(
+                first_name=body.get('first_name'),
+                last_name=body.get('last_name'),
+                username=body.get('username'),
+                email=body.get('email'),
+                password=body.get('password'),
+                is_teacher=True,
+            )
+            user.save()
+            groups = Group.objects.get(name='Teacher')
+            groups.user_set.add(user)
+            groups.save()
+            return JsonResponse({'data': self._serializer(user)}, status=201)
+        except IntegrityError as e:
+            print(e)
+            return JsonResponse({'massage': f'cannot create course. reason : {e}'}, status=400)
+
+
+class StudentCreateView(View):
+    @staticmethod
+    def _serializer(user):
+        return {
+            'id': user.id,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'is_student': user.is_student,
+            'groups': [group.name for group in user.groups.all()]
+        }
+
+    def post(self, request):
+        body = json.loads(request.body.decode('utf-8'))
+        try:
+            user = User(
+                first_name=body.get('first_name'),
+                last_name=body.get('last_name'),
+                username=body.get('username'),
+                email=body.get('email'),
+                password=body.get('password'),
+                is_student=True,
+            )
+            user.save()
+            groups = Group.objects.get(name='Student')
+            groups.user_set.add(user)
+            groups.save()
+            return JsonResponse({'data': self._serializer(user)}, status=201)
+        except IntegrityError as e:
+            print(e)
+            return JsonResponse({'massage': f'cannot create course. reason : {e}'}, status=400)
